2662f531be0116eed4d9cfa91803822e9a496323
= <=>
2022-03-12 12:06:09 +0545

    media queries

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 // reusable variables
 let image = document.querySelector("#search_result img")
 let searchBox = document.querySelector("#content input");
-let atoz = [
-    "apple", "ball", "cat", "dog", "elephant", "fox", "goat", "horse",
-    "ink", "jug", "kite", "lion", "monkey", "nut", "orange", "pan", "queen", "rabbit",
-    "swing", "tiger", "umbrella", "van", "watch", "x-ray", "zoo"
-]
+
 function changeImage() {
     
     // changes image according to the search
