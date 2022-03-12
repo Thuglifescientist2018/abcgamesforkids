f1928c686392316c433f8a89038de2e0b63d30f9
= <=>
2022-03-12 11:27:28 +0545

    match exact word js, fix img width, replace unsplash with gif images

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // reusable variables
 let image = document.querySelector("#search_result img")
 let searchBox = document.querySelector("#content input");
-
+let atoz = [
+    "apple", "ball", "cat", "dog", "elephant", "fox", "goat", "horse",
+    "ink", "jug", "kite", "lion", "monkey", "nut", "orange", "pan", "queen", "rabbit",
+    "swing", "tiger", "umbrella", "van", "watch", "x-ray", "zoo"
+]
 function changeImage() {
     
     // changes image according to the search
-    image.setAttribute("src", "https://source.unsplash.com/720x480/?" + searchBox.value)
+    image.setAttribute("src", "./atoz/" + searchBox.value + ".gif");
     
 }
