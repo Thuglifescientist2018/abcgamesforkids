ca15fbea2d3e31ef3bab2b84c5ed666e98cdf84c
= <=>
2022-03-14 14:59:20 +0545

    feature: get image by drawing or writing word by hand

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 let image = document.querySelector("#search_result img")
 let searchBox = document.querySelector("#content input");
 
-function changeImage() {
+function changeImage(name) {
     
+    if(name) { 
+        console.log(name.toLowerCase())
+        image.setAttribute("src", "./AtoZ/" + name.toLowerCase() + ".gif");
+    }
+    else {
     // changes image according to the search
-    image.setAttribute("src", "./AtoZ/" + searchBox.value + ".gif");
-
+    image.setAttribute("src", "./AtoZ/" + searchBox.value.toLowerCase() + ".gif");
+    }
     
+}
+
+function getResult(result) {
+ 
+  let data = result.split(',')[0];
+  changeImage(data)
 }
